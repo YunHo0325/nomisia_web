c2ef56695ab2225310bed650c0083cf2ba2b0dca
Yunho0325 <dbsgh3025@gmail.com>
2024-12-06 13:50:48 +0900

    feat(redirect) : wasabi로 redirect되게 다운로드 링크 설정

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/nomisia-0.1.0-setup.exe',
+        destination: 'https://s3.us-west-1.wasabisys.com/nomisia.storage/nomisia-0.1.0-setup.exe',
+        permanent: true,
+      },
+      {
+        source: '/install_mac.sh',
+        destination: 'https://s3.us-west-1.wasabisys.com/nomisia.storage/install_mac.sh',
+        permanent: true,
+      },
+      {
+        source: '/install_linux.sh',
+        destination: 'https://s3.us-west-1.wasabisys.com/nomisia.storage/install.sh',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
